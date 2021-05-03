812b4c5b6023d6724cf7e23308cdce1c207a1c3c
LuisMedinaG <humberto.medina@alumno.udg.mx>
2021-05-03 18:14:46 -0500

    Use gob decoder

--- client/main.go
+++ client/main.go
@@ -3,11 +3,9 @@ package main
 import (
 	"bufio"
 	"fmt"
-	"github.com/jonfk/golang-chat/tcp/common"
-	"io"
+	"gob"
 	"log"
 	"net"
-	"os"
 )
 
 const (
@@ -17,24 +15,18 @@ const (
 )
 
 func main() {
-	tcpAddr, err := net.ResolveTCPAddr("tcp", CONN_HOST+":"+CONN_PORT)
+	conn, err := net.Dial(CONN_TYPE,CONN_HOST+":"+CONN_PORT)
 	if err != nil {
 		log.Fatal(err)
 	}
-
-	// Connect to server through tcp.
-	conn, err := net.DialTCP("tcp", nil, tcpAddr)
-	if err != nil {
-		log.Fatal(err)
-	}
+	
 	defer conn.Close()
-	go printOutput(conn)
-	writeInput(conn)
+	sendMessage(conn)
 }
 
-func writeInput(conn *net.TCPConn) {
+func sendMessage(conn *net.TCPConn) {
+
 	fmt.Print("Enter username: ")
-	// Read from stdin.
 	reader := bufio.NewReader(os.Stdin)
 	username, err := reader.ReadString('\n')
 	username = username[:len(username)-1]
@@ -47,27 +39,27 @@ func writeInput(conn *net.TCPConn) {
 		if err != nil {
 			log.Fatal(err)
 		}
-		err = common.WriteMsg(conn, username+": "+text)
-		if err != nil {
-			log.Println(err)
-		}
-	}
-}
-
-func printOutput(conn *net.TCPConn) {
-	for {
-
-		msg, err := common.ReadMsg(conn)
-		// Receiving EOF means that the connection has been closed
-		if err == io.EOF {
-			// Close conn and exit
-			conn.Close()
-			fmt.Println("Connection Closed. Bye bye.")
-			os.Exit(0)
-		}
+		err = gob.NewEncoder(conn).Encode(msg)
 		if err != nil {
 			log.Fatal(err)
 		}
-		fmt.Println(msg)
 	}
 }
+
+// func printOutput(conn *net.TCPConn) {
+// 	for {
+
+// 		msg, err := common.ReadMsg(conn)
+// 		// Receiving EOF means that the connection has been closed
+// 		if err == io.EOF {
+// 			// Close conn and exit
+// 			conn.Close()
+// 			fmt.Println("Connection Closed. Bye bye.")
+// 			os.Exit(0)
+// 		}
+// 		if err != nil {
+// 			log.Fatal(err)
+// 		}
+// 		fmt.Println(msg)
+// 	}
+// }
